3771e75f947a522421fc28467bccfb61b7e24514
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-06-13 18:57:23 +0100

    refrature code

---
+++ components/Tabs.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { tabs } from "@/lib/constants";
+
+type TabsProps = {
+  tabs: typeof tabs;
+  activeTab: typeof tabs[number];
+  setActiveTab: (tab: typeof tabs[number]) => void;
+}
+
+export default function Tabs({ tabs, activeTab, setActiveTab }: TabsProps) {
+  return (
+    <div className="relative flex  gap-4 border border-white/10 p-1 rounded-full bg-white/5 w-fit">
+      {tabs.map((tab) => (
+        <button
+          key={tab}
+          onClick={() => setActiveTab(tab)}
+          className={`capitalize cursor-pointer px-4 py-1 text-sm font-medium rounded-full relative z-10 transition ${
+            activeTab === tab ? "text-black" : "text-white/70"
+          }`}
+        >
+          {tab}
+          {activeTab === tab && (
+            <motion.div
+              layoutId="activeTab"
+              transition={{ type: "spring", stiffness: 300, damping: 30 }}
+              className="absolute inset-0 bg-white rounded-full -z-10"
+            />
+          )}
+        </button>
+      ))}
+    </div>
+  );
+}
